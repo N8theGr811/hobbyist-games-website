854c9589fa773ac3481f25221ebcd60a089da3f4
Nathan Markham <nathanmarkham@gmail.com>
2026-04-10 16:49:24 -0700

    feat: try V3 formal logo in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
     <header className="fixed top-0 left-0 right-0 z-50 flex items-center justify-between px-6 py-1.5 bg-espresso shadow-[0_2px_16px_rgba(59,42,31,0.25)] md:px-12">
       <a href="#" className="block -my-3">
         <Image
-          src="/brand/hobbyist-games-v1-hero-2048w.png"
+          src="/brand/hobbyist-games-v3-formal-2048w.png"
           alt="Hobbyist Games"
           width={180}
           height={72}
