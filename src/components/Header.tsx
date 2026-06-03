871ed042878a7f0535b770f42dd18c8a1fb39eef
Nathan Markham <nathanmarkham@gmail.com>
2026-06-02 20:07:59 -0700

    feat: dark-navy/gold inverted logo variant for header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
     <header className="fixed top-0 left-0 right-0 z-50 flex items-center justify-between px-6 py-1.5 bg-steam-navy shadow-[0_2px_16px_rgba(0,0,0,0.5)] border-b-2 border-steam-gold/40 md:px-12">
       <a href="#" className="block -my-3">
         <Image
-          src="/brand/hobbyist-games-v3-formal-2048w.png"
+          src="/brand/hobbyist-games-v3-dark-2048w.png"
           alt="Hobbyist Games"
           width={180}
           height={72}
